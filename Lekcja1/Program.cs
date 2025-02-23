272aabc1f7d185198964d6046301906149e3bf92
Sebastian Len <sebastianlen03@gmail.com>
2025-02-23 22:58:55 +0100

    kontynuacja typy danych

--- Lekcja1/Program.cs
+++ Lekcja1/Program.cs
@@ -37,21 +37,52 @@
 
             //Typy danych [liczby całkowite]
 
-            byte varByte = 3; // 0 - 255
-            Console.WriteLine(varByte);
-            Console.WriteLine($"Byte min: {byte.MinValue}, Byte max: {byte.MaxValue}");
+            //byte varByte = 3; // 0 - 255
+            //Console.WriteLine(varByte);
+            //Console.WriteLine($"Byte min: {byte.MinValue}, Byte max: {byte.MaxValue}");
 
-            short varShort = 30000; // -32 768 - 32 767
-            Console.WriteLine(varShort);
-            Console.WriteLine($"Byte min: {short.MinValue}, Byte max: {short.MaxValue}");
+            //short varShort = 30000; // -32 768 - 32 767
+            //Console.WriteLine(varShort);
+            //Console.WriteLine($"Byte min: {short.MinValue}, Byte max: {short.MaxValue}");
 
-            int varInt = 2000000000; // -2 147 483 648 - 2 147 483 647
-            Console.WriteLine(varInt);
-            Console.WriteLine($"Byte min: {int.MinValue}, Byte max: {int.MaxValue}");
+            //int varInt = 2000000000; // -2 147 483 648 - 2 147 483 647
+            //Console.WriteLine(varInt);
+            //Console.WriteLine($"Byte min: {int.MinValue}, Byte max: {int.MaxValue}");
 
-            long varLong = 2000000000000000000; // -9 223 372 036 854 775 808 - 9 223 372 036 854 775 807
-            Console.WriteLine(varLong);
-            Console.WriteLine($"Byte min: {long.MinValue}, Byte max: {long.MaxValue}");
+            //long varLong = 2000000000000000000; // -9 223 372 036 854 775 808 - 9 223 372 036 854 775 807
+            //Console.WriteLine(varLong);
+            //Console.WriteLine($"Byte min: {long.MinValue}, Byte max: {long.MaxValue}");
+
+            //sbyte varSByte = -100; // -128 - 127 dodanie do byte s oznacza, że można przypisać liczby ujemne
+            //Console.WriteLine(varSByte);
+            //Console.WriteLine($"Byte min: {sbyte.MinValue}, Byte max: {sbyte.MaxValue}");
+
+            //uint varUInt = 4000000000; // 0 - 4 294 967 295 dodanie u do nazwy zmiennej oznacza, że nie można przypisać liczb ujemnych
+            //Console.WriteLine(varUInt);
+            //Console.WriteLine($"Byte min: {uint.MinValue}, Byte max: {uint.MaxValue}");
+
+            //Typy danych [liczby zmiennoprzecinkowe]
+
+            //float varFloat = 3.14f; // 3,402823E+38 - 1,401298E-45
+            //Console.WriteLine(varFloat);
+            //Console.WriteLine($"float max: {float.MaxValue}, float min: {float.MinValue}");
+
+            //double varDouble = 3.14; // 1,79769313486232E+308 - 4,94065645841247E-324
+            //Console.WriteLine(varDouble);
+            //Console.WriteLine($"double max: {double.MaxValue}, double min: {double.MinValue}");
+
+            //decimal varDecimal = 3.14m; // 79,228 162,514 264 337 593 543 950 335
+            //Console.WriteLine(varDecimal);
+            //Console.WriteLine($"decimal max: {decimal.MaxValue}, decimal min: {decimal.MinValue}");
+
+            //Typy danych [znakowy]
+
+            //char varChar = 'A'; //tylko jeden znak
+            //char znakSpecjalny = '\n'; //nowa linia, po \ można używać znaków specjalnych które zostaną wypisane
+            //Console.WriteLine(varChar);
+
+            //string varString = "Hello World"; //dowolna ilość znaków
+            //Console.WriteLine(varString);
         }
     }
 }
